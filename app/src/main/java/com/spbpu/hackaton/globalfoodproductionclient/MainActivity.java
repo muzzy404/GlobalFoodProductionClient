046966d685fbe9a582650f153b96f9c21e7701c2
Daria Zelenova <muzzy404@gmail.com>
2018-04-28 17:34:17 +0300

    fragments

--- app/src/main/java/com/spbpu/hackaton/globalfoodproductionclient/MainActivity.java
+++ app/src/main/java/com/spbpu/hackaton/globalfoodproductionclient/MainActivity.java
@@ -1,13 +1,19 @@
 package com.spbpu.hackaton.globalfoodproductionclient;
 
+import android.app.FragmentManager;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
     private Toolbar toolbar;
+    private BottomNavigationView btmNavigation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +22,27 @@ public class MainActivity extends AppCompatActivity {
 
         toolbar = findViewById(R.id.main_toolbar);
         setSupportActionBar(toolbar);
+
+        btmNavigation = findViewById(R.id.btm_nav_view);
+        btmNavigation.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                android.support.v4.app.FragmentManager fragmentManager = getSupportFragmentManager();
+                FragmentTransaction transaction = fragmentManager.beginTransaction();
+                switch (item.getItemId()) {
+
+                    case(R.id.map_chart_item):
+                        transaction.replace(R.id.content, new MapFragment()).commit();
+                        return true;
+                    case(R.id.pie_chart_item):
+                        transaction.replace(R.id.content, new PieFragment()).commit();
+                        return true;
+                }
+
+                return false;
+            }
+        });
+        btmNavigation.setSelectedItemId(R.id.map_chart_item);
     }
 
     @Override
