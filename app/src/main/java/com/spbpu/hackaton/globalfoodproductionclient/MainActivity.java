64454d7cf92955a3b41ed037038d0ce5a03747c0
Daria Zelenova <muzzy404@gmail.com>
2018-04-28 16:27:10 +0300

    app template

--- app/src/main/java/com/spbpu/hackaton/globalfoodproductionclient/MainActivity.java
+++ app/src/main/java/com/spbpu/hackaton/globalfoodproductionclient/MainActivity.java
@@ -2,12 +2,25 @@ package com.spbpu.hackaton.globalfoodproductionclient;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toolbar = findViewById(R.id.main_toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main_toolbar, menu);
+        return super.onCreateOptionsMenu(menu);
     }
 }
